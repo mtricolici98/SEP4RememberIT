5ad71cb2bb19531e1cab217ee8b5ee0445c7299a
nikovlyubomir <nikovlyubomir@gmail.com>
2018-05-23 14:20:33 +0200

    UpdateConnectionPanelsPlusDifficultyMenu

---
+++ Assets/Scripts/TransferPanelScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TransferPanelScript : MonoBehaviour {
+
+
+	[SerializeField] 	
+	public Button button;
+	[SerializeField] 	
+	private GameObject panel;
+	[SerializeField]
+	public GameObject paneltwo;
+
+		void Start()
+		{
+		
+		
+		
+		button.onClick.AddListener(TaskOnClick);
+
+		}
+
+		void TaskOnClick()
+		{
+		
+		paneltwo.SetActive (true);
+		panel.SetActive (false);
+		}
+
+		void TaskWithParameters(string message)
+		{
+			//Output this to console when the Button is clicked
+			Debug.Log(message);
+		}
+	}
+ 	
